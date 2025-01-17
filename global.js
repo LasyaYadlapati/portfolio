bd3737d7df320921379c30a9d35c8abb915eb2a2
Lasya Yadlapati <syadlapati@ucsd.edu>
2025-01-17 15:19:23 -0800

    reverted to old nav

--- global.js
+++ global.js
@@ -3,25 +3,3 @@ console.log("IT’S ALIVE!");
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
-
-let pages = [
-  { url: "/portfolio/index.html", title: "Home" },
-  { url: "/portfolio/contact/index.html", title: "Contact" },
-  { url: "/portfolio/projects/index.html", title: "Projects" },
-  { url: "/portfolio/resume/index.html", title: "Resume" },
-  { url: "https://github.com/LasyaYadlapati", title: "GitHub Profile" },
-];
-
-const ARE_WE_HOME = document.documentElement.classList.contains("home");
-if (!ARE_WE_HOME && !url.startsWith("http")) {
-  url = "../" + url;
-}
-
-let nav = document.createElement("nav");
-document.body.prepend(nav);
-
-for (let p of pages) {
-  let url = p.url;
-  let title = p.title;
-  nav.insertAdjacentHTML("beforeend", `<a href="${url}">${title}</a>`);
-}
