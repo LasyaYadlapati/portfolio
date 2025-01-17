b9031a3494994f5cd551e01fcb1ccd70dd76aa53
Lasya Yadlapati <syadlapati@ucsd.edu>
2025-01-17 15:40:50 -0800

    re-add nav

--- global.js
+++ global.js
@@ -18,5 +18,13 @@ document.body.prepend(nav);
 for (let p of pages) {
   let url = p.url;
   let title = p.title;
-  nav.insertAdjacentHTML("beforeend", `<a href="${url}">${title}</a>`);
+  let a = document.createElement("a");
+  a.href = url;
+  a.textContent = title;
+  nav.append(a);
+}
+
+const ARE_WE_HOME = document.documentElement.classList.contains("home");
+if (!ARE_WE_HOME && !url.startsWith("http")) {
+  url = "../" + url;
 }
