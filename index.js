8fef2da284b4d843eaaa07ac01ef87aa4c883ab4
Lasya Yadlapati <syadlapati@ucsd.edu>
2025-01-27 15:18:37 -0800

    minor edits to appearance

--- index.js
+++ index.js
@@ -10,11 +10,10 @@ const profileStats = document.querySelector("#profile-stats");
 
 if (profileStats) {
   profileStats.innerHTML = `
-    <dl>
-      <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
-      <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
-      <dt>Followers:</dt><dd>${githubData.followers}</dd>
-      <dt>Following:</dt><dd>${githubData.following}</dd>
-    </dl>
-  `;
+        <dl>
+          <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
+          <dt>Followers:</dt><dd>${githubData.followers}</dd>
+          <dt>Following:</dt><dd>${githubData.following}</dd>
+        </dl>
+    `;
 }
