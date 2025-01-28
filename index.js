8304b38bca8e9ce50b5836b37721f6a6023d747b
Lasya Yadlapati <syadlapati@ucsd.edu>
2025-01-27 17:57:20 -0800

    added latest projects and github stats

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-import { fetchJSON, renderProjects } from "../portfolio/global.js";
+import {
+  fetchJSON,
+  renderProjects,
+  fetchGitHubData,
+} from "../portfolio/global.js";
 const projects = await fetchJSON("../portfolio/lib/projects.json");
-const latestProjects = projects.slice(0, 3);
+const latestProjects = projects.slice(0, 2);
 
 const projectsContainer = document.querySelector(".projects");
 renderProjects(latestProjects, projectsContainer, "h2");
@@ -11,9 +15,10 @@ const profileStats = document.querySelector("#profile-stats");
 if (profileStats) {
   profileStats.innerHTML = `
         <dl>
-          <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
-          <dt>Followers:</dt><dd>${githubData.followers}</dd>
-          <dt>Following:</dt><dd>${githubData.following}</dd>
+          <dt>PUBLIC REPOS</dt><dd>${githubData.public_repos}</dd>
+          <dt>PUBLIC GISTS</dt><dd>${githubData.public_gists}</dd>
+          <dt>FOLLOWERS</dt><dd>${githubData.followers}</dd>
+          <dt>FOLLOWING</dt><dd>${githubData.following}</dd>
         </dl>
     `;
 }
